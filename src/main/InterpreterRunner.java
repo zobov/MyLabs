54f7b84520a9d72a7f3d478981fc5fcb0e1823cb
zobov <zobov.java@yandex.ru>
2014-05-08 10:54:22 -0700

    better works with exceptions

--- src/main/InterpreterRunner.java
+++ src/main/InterpreterRunner.java
@@ -1,4 +1,6 @@
-package main;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+
 /**
  * Created by Zobov on 17.04.14.
  */
@@ -7,11 +9,14 @@ public class InterpreterRunner {
     public static void main(String[] args) {
         if (args.length != 2) {
             System.err.println("You must choose <input file path> <output file path>");
+            return;
         }
         try {
             new Interpreter(args[0], args[1]).run();
-        } catch (Exception e) {
+        } catch (IOException | DataFormatException e) {
             System.err.print(e.getMessage());
+        } catch (Exception e) {
+            System.err.print(e);
         }
     }
 }
