c1f9959d139a8cb6110f05d4f0c06f72e3e16d22
zobov <zobov.java@yandex.ru>
2014-05-22 02:50:11 -0700

    Create InvalidCommandException.java

---
+++ src/main/InvalidCommandException.java
@@ -0,0 +1,10 @@
+package main;
+
+public class InvalidCommandException extends Exception {
+	public InvalidCommandException(){
+	}
+	
+	public InvalidCommandException(String message){
+		super(message);
+	}
+}
