1be23e960e33357390d7519be5a94aa748db831a
zobov <zobov.java@yandex.ru>
2014-05-28 08:52:36 -0700

    Update Manager.java

--- src/main/Manager.java
+++ src/main/Manager.java
@@ -6,63 +6,38 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-
+import java.util.concurrent.TimeUnit;
 
 public class Manager {
 
-	public static void main(String[] Args) {
-		List<Integer> maxValues = Collections.synchronizedList(new ArrayList<Integer>());
-		ExecutorService es = Executors.newFixedThreadPool(3);
-		List<String> data;
-		try {
-			data = getData(Args[0]);	
-			for (String s : data) {
-				es.execute(new Maximum(parseData(s), maxValues));
-			}			
-			es.shutdown();		
-			while(!es.isTerminated());	
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+			List<Integer> maxValues = Collections.synchronizedList(new ArrayList<Integer>());
+			ExecutorService es = Executors.newFixedThreadPool(3);
+			while (br.ready()) {
+				String inputData = br.readLine();
+				if (inputData.isEmpty()) {
+					continue;
+				}
+				es.execute(new Maximum(inputData, maxValues));
+			}
+			es.shutdown();
+			boolean isTerminated = false;
+			while (!isTerminated) {
+				isTerminated = es.awaitTermination(1, TimeUnit.HOURS);
+			}
 			if (maxValues.isEmpty()) {
 				System.out.println("Inputfile was empty");
 				return;
-			} 
+			}
 			int result = maxValues.get(0);
 			for (int temp : maxValues) {
 				result = Math.max(temp, result);
 			}
 			System.out.println("Max value = " + result);
 
-		} catch (Exception e) {
+		} catch (InterruptedException | IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	private static List<String> getData(String fileName) throws IOException {
-		BufferedReader br = null;
-		List<String> list = new ArrayList<String>();
-		try {
-			br = new BufferedReader(new FileReader(fileName));
-			while(br.ready()) {
-				list.add(br.readLine());
-			}
-
-		} finally {
-			try {
-				if (br != null) br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return list;
-	}
-	
-	private static int[] parseData(String data) {
-		String[] args = data.split(" ");
-		int[] mas = new int[args.length];
-		int i = 0;
-		for (String s : args) {
-			mas[i++] = Integer.parseInt(s);
-		}			
-		return mas;
 	}
 }
